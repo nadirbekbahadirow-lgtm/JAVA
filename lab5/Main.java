1a40341c31b62e545b49aca4e2cc923e73240b8c
Bahadirow nadirbek <nadirbekbahadirow@gamil.com>
2026-02-24 12:57:01 +0500

    lab5

---
+++ lab5/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+
+        while (number <= 0) {
+            System.out.print("Оң сан енгізіңіз: ");
+            number = scanner.nextInt();
+        }
+
+        System.out.println("Рахмет! Сіз енгізген оң сан: " + number);
+    }
+}
